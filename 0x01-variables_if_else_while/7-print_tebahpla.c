d57586a66b8cbd6ff8c2a4a225403938229c32e4
RichieKwaku <fellowrbj@gmail.com>
2023-07-17 18:34:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char r;
+
+	r = 'z';
+	while
+		(r >= 'a') {
+			putchar(r);
+			r--;
+		}
+	putchar('\n');
+	return (0);
+}
