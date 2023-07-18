5911ca1c3e82718bc8a9acde5686e9efd5e2e28f
RichieKwaku <fellowrbj@gmail.com>
2023-07-17 18:08:12 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,12 @@ int main(void)
 
 	c = 'a';
 	while
-		(c <= 'z')
+		(c <= 'z') {
 			if ((c != 'q' && c != 'e') && c <= 'z')
 				putchar(c);
-	c++;
+			c++;
+		}
+
 	putchar('\n');
 	return (0);
 }
