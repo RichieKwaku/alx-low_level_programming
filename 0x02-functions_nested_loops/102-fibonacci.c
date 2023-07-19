4dedbb8f193d4ebea3c04fdc9b10bba7e59f15a0
RichieKwaku <fellowrbj@gmail.com>
2023-07-19 16:50:18 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	long int i, j, k, next;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	return (0);
+}
