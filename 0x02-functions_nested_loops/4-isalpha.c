c942a3511851552914ef13c4ef9563f727bf692b
RichieKwaku <fellowrbj@gmail.com>
2023-07-18 19:07:06 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - alpha chars
+ * @c: chars to be checked
+ * Return: 1 for alpha chars 0 for anything
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 65 && c >= 97) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
