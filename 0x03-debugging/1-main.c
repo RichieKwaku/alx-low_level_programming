c6ef9ad35068141d7ab675e0453c0b87d9751a4b
RichieKwaku <fellowrbj@gmail.com>
2023-07-20 12:13:19 -0700

    Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - C debugging
+ * Return: 0 
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0
+
+		/*while (i < 10)*/
+		/*{*/
+		/*   putchar(i);*/
+		/*}*/
+
+		printf("infinite loop avioded! \\o/\n");
+
+	return (0);
+}
+
+
